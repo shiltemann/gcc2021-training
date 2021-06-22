dc1d06d6711f23d85a6b376c95f6b23cbb2cca74
Helena Rasche <hxr@hx42.org>
2021-06-22 15:50:02 +0200

    done?

--- slack-bot/extra/build.sh
+++ slack-bot/extra/build.sh
@@ -1,9 +1,49 @@
 #!/bin/bash
-start="2021-02-14 11:09:25+01:00"
-
-for day in {15..18}; do
+# Welcome messages
+for course in {gtn,gat}; do
 	for region in {Pacific/Auckland,Europe/Amsterdam,America/New_York}; do
-		sendafter=$(TZ=$region date -d "2021-02-$day 18:00:00" --rfc-3339=seconds | sed 's/ /T/')
-		python3 template.py $region shift-change-template.json > "scheduled/${sendafter}-shift-change.json"
+		sendafter=$(TZ=$region date -d "2021-06-28 8:00:00" --rfc-3339=seconds | sed 's/ /T/')
+		python3 extras/template.py $region extras/welcome-${course}.json $course | \
+			sed 's/ICEBREAKER/Let us know where you are from, and one fun fact about yourself!/g' | \
+			> "scheduled/${course}/${sendafter}-welcome.json"
+	done
+done
+
+# Shift change messages
+for course in {gtn,gat}; do
+	for region in {Pacific/Auckland,Europe/Amsterdam,America/New_York}; do
+		sendafter=$(TZ=$region date -d "2021-06-29 10:00:00" --rfc-3339=seconds | sed 's/ /T/')
+		python3 extras/template.py $region extras/shift-change-${course}.json $course | \
+			sed 's/ICEBREAKER/What is the coolest, most mind blowing fact (nature/people/animal etc.) you know?/g' | \
+			> "scheduled/${course}/${sendafter}-shift-change.json"
+	done
+done
+
+for course in {gtn,gat}; do
+	for region in {Pacific/Auckland,Europe/Amsterdam,America/New_York}; do
+		sendafter=$(TZ=$region date -d "2021-06-30 10:00:00" --rfc-3339=seconds | sed 's/ /T/')
+		python3 extras/template.py $region extras/shift-change-${course}.json $course | \
+			sed 's/ICEBREAKER/What is your favorite dish (food or drink)? Bonus points for recipes!/g' | \
+			> "scheduled/${course}/${sendafter}-shift-change.json"
+	done
+done
+
+for course in {gtn,gat}; do
+	for region in {Pacific/Auckland,Europe/Amsterdam,America/New_York}; do
+		sendafter=$(TZ=$region date -d "2021-07-01 10:00:00" --rfc-3339=seconds | sed 's/ /T/')
+		python3 extras/template.py $region extras/shift-change-${course}.json $course | \
+			sed 's/ICEBREAKER/If you had a time machine, what is the first place & time you would visit? why?/g' | \
+			> "scheduled/${course}/${sendafter}-shift-change.json"
+	done
+done
+
+
+# Goodbye
+for course in {gtn,gat}; do
+	for region in {Pacific/Auckland,Europe/Amsterdam,America/New_York}; do
+		sendafter=$(TZ=$region date -d "2021-07-02 12:00:00" --rfc-3339=seconds | sed 's/ /T/')
+		python3 extras/template.py $region extras/goodbye-${course}.json $course | \
+			sed 's/ICEBREAKER/What has been you favourite part of this training week? And what are you looking forward to most during the rest of GCC (if you are attending)?/g' | \
+			> "scheduled/${course}/${sendafter}-goodbye.json"
 	done
 done
--- slack-bot/extra/template.py
+++ slack-bot/extra/template.py
@@ -4,19 +4,26 @@ import json
 
 region = sys.argv[1]
 fn = sys.argv[2]
+course = sys.argv[3]
 
 replacements = {
     'Pacific/Auckland': {
-        'WELCOME_REGION': '*Good morning/afternoon Africa, Middle East, and Europe!* :earth_africa:',
-        'BYE_REGION': 'Asia/Pacific :earth_asia: region helpers are ending their workday, so it\'s over to Middle East/Europe/Africa :earth_africa:!'
+        'REGION': 'Asia/Pacific :earth_asia:',
+        'BYE_REGION': 'Americas :earth_americas:'
     },
     'Europe/Amsterdam': {
-        'WELCOME_REGION': 'Good Morning/Afternoon South, Central, and North America! :earth_americas:',
-        'BYE_REGION': 'Europe/Middle East/Africa :earth_africa: region helpers are ending their workday, so it\'s over to the Americas :earth_americas:!'
+        'REGION': 'Africa/Middle East/Europe :earth_africa:',
+        'BYE_REGION': 'Asia/Pacific :earth_asia:'
     },
     'America/New_York': {
-        'WELCOME_REGION': '*Good morning/afternoon Asia/Pacific!* :earth_asia:',
-        'BYE_REGION': 'Americas :earth_americas: region helpers are ending their workday, so it\'s over to Asia/Pacific :earth_asia:!'
+        'REGION': 'Americas :earth_americas:',
+        'BYE_REGION': 'Europe/Middle East/Africa :earth_africa:'
+    },
+    'gat': {
+        'SOCIAL_CHANNEL': '#general'
+    },
+    'gtn': {
+        'SOCIAL_CHANNEL': '#social'
     }
 }
 
@@ -24,6 +31,9 @@ with open(fn, 'r') as handle:
     data = handle.read()
 
 for k, v in replacements[region].items():
+    data = data.replace(f'<{k}>', v)
+
+for k, v in replacements[course].items():
     data = data.replace(f'<{k}>', v)
 
 print(data)
